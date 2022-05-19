6193a8fe3bb2eee9004b21f7276c28e05a9c4c1e
lijialiang <lijialiang@guorou.tv>
2022-05-19 17:18:38 +0800

    demo

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,30 @@
-const tasks = [
-    {
-        id: 1,
-        text:'doctors appointments',
-        days:'feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text:'meeting at school',
-        days:'feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text:'food shopping',
-        days:'feb 5th at 2:30pm',
-        reminder: false,
-    },
+import {useState} from 'react'
 
-];
 
 export const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text:'doctors appointments',
+            days:'feb 5th at 2:30pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text:'meeting at school',
+            days:'feb 6th at 1:30pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text:'food shopping',
+            days:'feb 5th at 2:30pm',
+            reminder: false,
+        },
+    
+    ])
   return (
+     
     <>
     {tasks.map((task) =>(<h3 key={task.id} >{task.text}</h3>))}
     </>
